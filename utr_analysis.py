b090f11945aabfeb0e80bf97576017c3227f70ad
Keith Hughitt <keith.hughitt@gmail.com>
2013-12-16 07:32:31 -0500

    Added an initial parse_reads function

--- utr_analysis.py
+++ utr_analysis.py
@@ -17,16 +17,63 @@ NOTE:
 
 Initially, development of this script will focus on SL site determination. Once
 this has been properly implemented, support for poly-A analysis will be added.
+
+Testing:
+
+utr_analysis.py \
+    -i $RAW/tcruzir21/HPGL0258/processed/*.filtered.fastq \ 
+    -f $REF/tcruzi_clbrener/genome/tc_esmer/TriTrypDB-6.0_TcruziCLBrenerEsmeraldo-like_Genome.fasta \
+    -g $REF/tcruzi_clbrener/annotation/tc_esmer/TriTrypDB-6.0_TcruziCLBrenerEsmeraldo-like.gff      \
+    -s AACTAACGCTATTATTGATACAGTTTCTGTACTATATTG output.csv
+
 """
 import os
 import sys
 import re
+import glob
 import textwrap
 import argparse
 
 def main():
     """Main"""
     args = parse_input()
+    matches = parse_reads(args)
+
+    # save output to a file for now
+    with open(args.output, 'w') as fp:
+        fp.writelines(matches)
+
+def parse_reads(args):
+    """
+    Loads a collection of RNA-Seq reads and filters the reads so as to only
+    return those containing the feature of interest (SL or polyA tail).
+    """
+    matches = []
+
+    # limit to matches of size min_length or greater
+    s = spliced_leader[-args.min_length:]
+
+    # compile regex allowing a single mismatch;
+    # we will filter based on number of mistmatches again later on a larger
+    # portion of the read using the value specified at run-time
+    regex = re.compile('|'.join('^%s.%s' % (s[:i], s[i+1:]) for i in range(len(s))))
+
+    for filepath in glob.glob(args.input_reads):
+        # open fastq file
+        fastq = open(filepath)
+        print("Processing %s" % filepath)
+
+        # filter by length
+        for i, read in enumerate(readfq(fastq)):
+            seq = read[1][:len(args.spliced_leader)]
+
+            # check for match
+            if re.search(regex, seq) is not None:
+                matches.append([i, seq])
+
+    # filtered sequeneces
+    return matches
+
 
 def parse_input():
     """
@@ -53,22 +100,33 @@ def parse_input():
     )
 
     # Add arguments
-    parser.add_argument('-i', '--input-reads',
+    parser.add_argument('-i', '--input-reads', required=True,
                         help='RNA-Seq FASTQ glob string')
+    parser.add_argument('-f', '--fasta-genome', dest='fasta', required=True,
+                        help='Genome sequence FASTA filepath')
+    parser.add_argument('-g', '--gff-annotation', dest='gff', required=True,
+                        help='Genome annotation GFF')
     parser.add_argument('-s', '--sl-sequence', dest='spliced_leader', 
                         help='Spliced leader DNA sequence')
-    parser.add_argument('-f', '--fasta-genome', dest='fasta',
-                        help='Genome sequence FASTA filepath')
-    parser.add_argument('-g', '--gff-annotation', dest='gff',
-                        help='Genome annotation GFF')
-    parser.add_argument('-m', '--min-length', default=10,
+    parser.add_argument('-m', '--min-length', default=10, type=int,
                         help='Minimum length of SL match (default=10)')
-    parser.add_argument('-n', '--num-mismatches', default=2,
+    parser.add_argument('-n', '--num-mismatches', default=2, type=int,
                         help='Number of mismatches to allow (default=2)')
     parser.add_argument('output', metavar='OUTPUT FILENAME',
                         help='Filepath to write CSV output to.')
 
-    return parser.parse_args()
+    # Parse arguments
+    args = parser.parse_args()
+
+    # Replace any environmental variables and return args
+    args.fasta = os.path.expandvars(args.fasta)
+    args.input_reads = os.path.expandvars(args.input_reads)
+    args.gff = os.path.expandvars(args.gff)
+    args.output = os.path.expandvars(args.output)
+
+    # @TODO Validate input
+
+    return args
 
 def readfq(fp):
     """
